249af4174bf57fadbae5ca93b17cc05130067a0f
Leomir <leomir2002@gmail.com>
2025-02-05 14:31:33 +0200

    worklow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
 }
 
 dependencies {
+    implementation ("org.apache.commons:commons-compress:1.27.1")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
